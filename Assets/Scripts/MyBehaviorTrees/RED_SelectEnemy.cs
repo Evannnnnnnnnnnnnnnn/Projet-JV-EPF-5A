13b8ade81b7807b6a184db7230b86254ffea3100
Evan <evan.pasteau@gmail.com>
2025-11-04 14:07:59 +0100

    All enemy in not null

---
+++ Assets/Scripts/MyBehaviorTrees/RED_SelectEnemy.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using System.Collections.Generic;
+
+[TaskCategory("MyTasks")]
+[TaskDescription("Select non targeted enemy Drone")]
+
+public class RED_SelectEnemy : Action
+{
+	IArmyElement m_ArmyElement;
+	public SharedTransform target;
+	public SharedFloat minRadius;
+	public SharedFloat maxRadius;
+    List<ArmyElement> _AllEnemies;
+    bool _isInitialized = false; // Renommé pour plus de clarté
+
+	public override void OnAwake()
+	{
+        m_ArmyElement = (IArmyElement) GetComponent(typeof(IArmyElement));
+	}
+
+	public override TaskStatus OnUpdate()
+    {
+        // Tant que la référence n'est pas injectée par ArmyManager, on attend.
+        if (m_ArmyElement.ArmyManager == null) 
+        {
+            // On retourne "Running" pour que la tâche continue d'essayer à la frame suivante
+            return TaskStatus.Running;
+        }
+
+        // On exécute cela seulement si le manager est prêt ET si on ne l'a pas déjà fait.
+        if (!_isInitialized)
+        {
+            _AllEnemies = m_ArmyElement.ArmyManager.GetAllEnemies(false);
+            _isInitialized = true; // On marque comme initialisé
+        }
+
+        if (target.Value == null)
+        {
+            _AllEnemies.RemoveAll(item => item == null);
+
+            if (_AllEnemies.Count > 0)
+            {
+                target.Value = _AllEnemies[0].transform;
+            }
+            else
+            {
+                return TaskStatus.Failure;
+            }
+        }
+
+        return TaskStatus.Success;
+	}
+}
